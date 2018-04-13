c1c338d369df4fed89a055fc503c3b33ba1515f7
ScottWorks <scottworks.io@gmail.com>
2018-04-13 09:41:44 -0600

    client setup complete

--- houser/src/App.js
+++ houser/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header';
 import Dashboard from './components/Dashboard/Dashboard';
-import House from './components/House/House';
 import Wizard from './components/Wizard/Wizard';
 import './App.css';
 
@@ -11,7 +10,6 @@ class App extends Component {
       <div className="App">
         <Header />
         <Dashboard />
-        <House />
         <Wizard />
       </div>
     );
--- houser/src/components/Dashboard/Dashboard.js
+++ houser/src/components/Dashboard/Dashboard.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import House from '../../components/House/House';
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
         <h1>Dashboard</h1>
+        <House />
       </div>
     );
   }
