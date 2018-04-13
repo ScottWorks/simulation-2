8299829a0a6328e0a81719af20a68c42187072b0
ScottWorks <scottworks.io@gmail.com>
2018-04-13 10:00:25 -0600

    database setup completed

--- houser/server/index.js
+++ houser/server/index.js
@@ -1,8 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const massive = require('massive');
 
 const app = express();
 app.use(bodyParser.json());
 
 const port = 4000;
-app.listen(port, () => console.log(`Listening on port: ${port}`));
+massive(process.env.CONNECTION_URI).then((dbInstance) => {
+  app.set('db', dbInstance);
+  app.listen(port, () => console.log(`Listening on port: ${port}`));
+});
